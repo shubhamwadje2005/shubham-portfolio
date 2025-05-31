404068183907710bc0bc198467f61cda4287e482
SHUBHAM WADJE <shubhamwadje2005@gmail.com>
2025-05-31 16:08:13 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
+const path = require("path");
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.static("dist"));
+app.use(cors({ origin: "https://shubham-portfolio-vgfs.onrender.com", credentials: true }))
 
 app.use("/api/contact", require("./routes/contact.route"))
 
+
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found" })
-})
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
     console.log(err)
